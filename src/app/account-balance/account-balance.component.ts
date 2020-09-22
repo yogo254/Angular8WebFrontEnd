42de92970b5c4733461df07b1e49a3de1b60fe99
yogo254 <yogo254@yahoo.com>
2020-09-22 21:27:59 +0300

    fix search bug

--- src/app/account-balance/account-balance.component.ts
+++ src/app/account-balance/account-balance.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit, OnChanges } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { Account } from "../service/model/Account";
 import { SearchService } from "../service/SearchService";
 import { AuthService } from "../service/AuthService";
-import { EventEmitter } from "protractor";
 
 @Component({
   selector: "app-account-balance",
   templateUrl: "./account-balance.component.html",
   styleUrls: ["./account-balance.component.scss"],
 })
-export class AccountBalanceComponent implements OnInit, OnChanges {
+export class AccountBalanceComponent implements OnInit {
   account = new Account();
 
   constructor(
